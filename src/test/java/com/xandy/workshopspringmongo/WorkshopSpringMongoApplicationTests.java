521f7fe393f98a9ff9666b0de0b242da496e68e7
xandy <>
2025-01-16 17:04:14 -0400

    /start: init project

---
+++ src/test/java/com/xandy/workshopspringmongo/WorkshopSpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xandy.workshopspringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
